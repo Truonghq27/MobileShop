c2f38d6a5abc59d9091f53f7c819baab71126a45
Hqtruong27 <hqtruong27@gmail.com>
2020-10-08 00:05:43 +0700

    default role and default account adnin 'read more in readme.md

--- Models/Migrations/Configuration.cs
+++ Models/Migrations/Configuration.cs
@@ -19,6 +19,16 @@ namespace Models.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+            var administator = context.Users.Any(u => u.IsAdmin == true);
+            if (administator == false)
+            {
+                context.Users.Add(new Models.DataModels.User { UserName = "Admin", FullName = "Hoang Truong", Email = "hqtruong27@gmail.com", Password = "$2a$10$taMaQNurgzOppm.X6vgtYO8xjtFGunf9wg.QD57WO02j4PcMMOJVa", Phone = "0963712001", Avatar = "", IsAdmin = true, Status = 1, mStatus = 1, isEmailVerified = true, ActiveCode = new Guid(), ResetPasswordCode = "", GroupId = null });
+            }
+            context.Roles.AddOrUpdate(new Models.DataModels.Role { RoleId = "VIEW", RoleName = "VIEW", Status = 1 });
+            context.Roles.AddOrUpdate(new Models.DataModels.Role { RoleId = "CREATE", RoleName = "CREATE", Status = 1 });
+            context.Roles.AddOrUpdate(new Models.DataModels.Role { RoleId = "UPDATE", RoleName = "UPDATE", Status = 1 });
+            context.Roles.AddOrUpdate(new Models.DataModels.Role { RoleId = "DELETE", RoleName = "DELETE", Status = 1 });
+            context.SaveChanges();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,15 +1,4 @@
 # MobileShop
-1. Create 1 Users Manager in SQL Server<br>
-UserName: Your User<br>
-FullName: Your name<br>
-Email: Your email<br>
-Phone: you phone<br>
-Avatar: null<br>
-IsAdmin true<br>
-status: 1<br>
-mStatus: 1<br>
-isEmailVerified: 0<br>
-ActiveCode: 00000000-0000-0000-0000-000000000000<br>
-ResetPwd: null<br>
-GroupId: null<br>
 2. Login admin with link localhost/admin/dang-nhap
+Email: hqtruong27@gmail.com
+pwd: 111111
